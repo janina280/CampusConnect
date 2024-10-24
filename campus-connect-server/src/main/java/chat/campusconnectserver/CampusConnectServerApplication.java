94c6962f305c0bed370bb7cd1c4cc776e11f2b6a
janina280 <coceijanina@yahoo.com>
2024-10-24 14:06:06 +0300

    create project structure

--- campus-connect-server/src/main/java/chat/campusconnectserver/CampusConnectServerApplication.java
+++ campus-connect-server/src/main/java/chat/campusconnectserver/CampusConnectServerApplication.java
@@ -1,4 +1,4 @@
-package chat.CampusConnectServer;
+package chat.campusconnectserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampusConnectServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CampusConnectServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CampusConnectServerApplication.class, args);
+    }
 
 }
--- campus-connect-server/src/test/java/chat/campusconnectserver/CampusConnectServerApplicationTests.java
+++ campus-connect-server/src/test/java/chat/campusconnectserver/CampusConnectServerApplicationTests.java
@@ -1,4 +1,4 @@
-package chat.CampusConnectServer;
+package chat.campusconnectserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CampusConnectServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
