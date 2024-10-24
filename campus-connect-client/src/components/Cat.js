f1ae57ec99d6cc91711149a9efd7a4da941e424e
janina280 <coceijanina@yahoo.com>
2024-10-24 18:21:13 +0300

    sidebar

---
+++ campus-connect-client/src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src="https://i.pinimg.com/736x/c2/6e/e4/c26ee41e4c5778e84aa835aef082f8c4.jpg"/>
+  )
+}
+
+export default Cat
--- campus-connect-client/src/layouts/dashboard/index.js
+++ campus-connect-client/src/layouts/dashboard/index.js
@@ -1,11 +1,33 @@
 import React from "react";
+import {Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { Outlet } from "react-router-dom";
+import Logo  from "../../assets/Images/logo.ico";
 
 const DashboardLayout = () => {
+const theme= useTheme();
 
   return (
     <>
-      Dashboard Layout
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+            height: 64,
+            width:64,
+            borderRadius:1.5,
+          }}
+        >
+          <img src={Logo}/>
+        </Box>
+      </Box>
       <Outlet />
     </>
   );
