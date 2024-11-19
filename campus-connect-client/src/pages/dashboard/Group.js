a610171e60123d98db670a24600d81ab587ab355
janina280 <coceijanina@yahoo.com>
2024-11-20 00:06:46 +0200

    create box

--- campus-connect-client/src/pages/dashboard/Group.js
+++ campus-connect-client/src/pages/dashboard/Group.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useTheme } from "@mui/material/styles"; // Correct useTheme import
+import { Box, Stack } from "@mui/material";
 
 const Group = () => {
-  return (
-    <div>Group</div>
-  )
-}
+  const theme = useTheme();
 
-export default Group
+  return (
+    <>
+      <Stack direction="row" sx={{ width: "100%" }}>
+        {/* Left */}
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: theme.palette.mode === "light" 
+              ? "#F8FAFF" 
+              : theme.palette.background.default, 
+          }}
+        >
+          {/* Add content here */}
+        </Box>
+        {/* Right */}
+        <Box sx={{ flex: 1, backgroundColor: theme.palette.background.paper }}>
+          {/* Right content */}
+        </Box>
+      </Stack>
+    </>
+  );
+};
+
+export default Group;
