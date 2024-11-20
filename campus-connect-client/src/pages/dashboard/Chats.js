a9dff4078cc7f8eabb61d6e698651cabc7db3a46
janina280 <coceijanina@yahoo.com>
2024-11-20 11:47:13 +0200

    group

--- campus-connect-client/src/pages/dashboard/Chats.js
+++ campus-connect-client/src/pages/dashboard/Chats.js
@@ -1,98 +1,19 @@
 import React from "react";
 import {
   Box,
-  Avatar,
   IconButton,
   Stack,
   Typography,
   Button,
-  Badge,
   Divider,
 } from "@mui/material";
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
-import { styled, useTheme } from "@mui/material/styles";
-import { faker } from "@faker-js/faker";
+import { useTheme } from "@mui/material/styles";
 import { ChatList } from "../../data";
 import { SimpleBarStyle } from "../../components/Scrollbar";
 import { Search, SearchIconWrapper, StyledInputBase } from "../../components/Search";
+import ChatElement from "../../components/ChatElement";
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    backgroundColor: "#44b700",
-    color: "#44b700",
-    boxShadow: `0 0 0 2px calc(1em + ${theme.palette.background.paper})`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      animation: "ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""',
-    },
-  },
-
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
-    },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
-}));
-
-const ChatElement = ({ id, name, img, msg, time, unread, online }) => {
-  const theme = useTheme();
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        borderRadius: 1,
-        backgroundColor:
-          theme.palette.mode === "light"
-            ? "#FFF"
-            : theme.palette.background.default,
-      }}
-      p={2}
-    >
-      <Stack
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Stack direction={"row"} spacing={2}>
-          {online ? (
-            <StyledBadge
-              overlap="circular"
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              variant="dot"
-            >
-              <Avatar src={faker.image.avatar()} />
-            </StyledBadge>
-          ) : (
-            <Avatar src={faker.image.avatar()} />
-          )}
-
-          <Stack spacing={0.3}>
-            <Typography variant="subtitle2">{name}</Typography>
-            <Typography variant="caption">{msg}</Typography>
-          </Stack>
-        </Stack>
-        <Stack spacing={2} alignItems={"center"}>
-          <Typography sx={{ fontWeight: 600 }} variant="caption">
-            {time}
-          </Typography>
-          <Badge color="primary" badgeContent={unread}></Badge>
-        </Stack>
-      </Stack>
-    </Box>
-  );
-};
 
 const Chats = () => {
   const theme = useTheme();
--- campus-connect-client/src/pages/dashboard/Group.js
+++ campus-connect-client/src/pages/dashboard/Group.js
@@ -2,11 +2,21 @@ import React from "react";
 import { useTheme } from "@mui/material/styles";
 import {
   Box,
+  Divider,
+  IconButton,
+  Link,
   Stack,
   Typography,
 } from "@mui/material";
-import {  MagnifyingGlass } from "phosphor-react";
-import { Search, SearchIconWrapper, StyledInputBase } from "../../components/Search";
+import { MagnifyingGlass, Plus } from "phosphor-react";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+import { ChatList } from "../../data";
+import ChatElement from "../../components/ChatElement";
 
 const Group = () => {
   const theme = useTheme();
@@ -28,25 +38,63 @@ const Group = () => {
           {/* Add content here */}
           <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
             <Stack>
-              <Typography variant='h5'>Groups</Typography>
+              <Typography variant="h5">Groups</Typography>
             </Stack>
-            <Stack sx={{width:"100%"}}>
-            <Search>
-            <SearchIconWrapper>
-              <MagnifyingGlass color="#709CE6" />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Stack>
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Typography variant="subtitle2" component={Link}>
+                Create New Group
+              </Typography>
+              <IconButton>
+                <Plus
+                  style={{ color: (theme) => theme.palette.primary.main }}
+                />
+              </IconButton>
+            </Stack>
+            <Divider />
+            <Stack
+              spacing={3}
+              sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}
+            >
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.5}>
+                  {/* */}
+                  <Typography variant="subtitle2" sx={{ color: "#676667" }}>
+                    Pinned
+                  </Typography>
+                  {/* Chat List */}
+                  {ChatList.filter((el) => el.pinned).map((el) => {
+                    return <ChatElement {...el} />;
+                  })}
+                  {/* */}
+                  <Typography variant="subtitle2" sx={{ color: "#676667" }}>
+                    All Groups
+                  </Typography>
+                  {/* Chat List */}
+                  {ChatList.filter((el) => !el.pinned).map((el) => {
+                    return <ChatElement {...el} />;
+                  })}
+                </Stack>
+              </SimpleBarStyle>
             </Stack>
           </Stack>
         </Box>
         {/* Right */}
-        <Box sx={{ flex: 1, backgroundColor: theme.palette.background.paper }}>
-          {/* Right content */}
-        </Box>
+       
       </Stack>
     </>
   );
