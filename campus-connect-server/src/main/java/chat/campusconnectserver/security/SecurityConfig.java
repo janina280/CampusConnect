ab9ae9af327c2e289334b1071f63c9ff058ebf04
janina280 <coceijanina@yahoo.com>
2024-12-04 23:52:22 +0200

    Create security config version 2

--- campus-connect-server/src/main/java/chat/campusconnectserver/security/SecurityConfig.java
+++ campus-connect-server/src/main/java/chat/campusconnectserver/security/SecurityConfig.java
@@ -2,25 +2,26 @@ package chat.campusconnectserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-http
-        .csrf()
-        .disable()
-        .authorizeHttpRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .oauth2Login()
-        ;
 
+        http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated() )
+        .formLogin(form -> form.defaultSuccessUrl("/hello", true) )
+                .oauth2Login(oauth2->oauth2.defaultSuccessUrl("/hello", true))
+        ;
 
         return  http.build();
     }
