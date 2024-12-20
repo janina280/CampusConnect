5cebc72e93ab94defa98c0e8acfef3081eacc1bd
janina280 <coceijanina@yahoo.com>
2024-12-20 12:06:50 +0200

    Remove class

--- campus-connect-server/src/main/java/chat/campusconnectserver/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package chat.campusconnectserver.config;
-
-public class JwtConstant {
-    public static final String JWT_HEADER = "Authorization";
-    public static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ";
-}
