4894fb4c82f41bfdf597701f7e53c8a881db1885
janina280 <coceijanina@yahoo.com>
2024-12-20 12:16:39 +0200

    Create exception

---
+++ campus-connect-server/src/main/java/chat/campusconnectserver/exception/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,12 @@
+package chat.campusconnectserver.exception;
+
+public class OAuth2AuthenticationProcessingException extends RuntimeException {
+
+    public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+}
