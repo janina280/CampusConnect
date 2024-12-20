a6bee337536a024dbbc22ae81231cce454042268
janina280 <coceijanina@yahoo.com>
2024-12-20 12:20:38 +0200

    Create security oauth2
    Signed-off-by: janina280 <coceijanina@yahoo.com>

--- campus-connect-server/src/main/java/chat/campusconnectserver/repositories/UserRepository.java
+++ campus-connect-server/src/main/java/chat/campusconnectserver/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package chat.campusconnectserver.repositories;
 
-import chat.campusconnectserver.models.User;
+import chat.campusconnectserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
