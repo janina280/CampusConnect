84dfe0115e07d01c4e08946809cb1bfaaeffd300
janina280 <coceijanina@yahoo.com>
2025-01-09 13:02:06 +0200

    Loading page

--- campus-connect-client/src/components/LoadingScreen.js
+++ campus-connect-client/src/components/LoadingScreen.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Box, CircularProgress } from "@mui/material";
 
 const LoadingScreen = () => {
-  return <>Loading...</>;
+  const theme = useTheme();
+  const [dots, setDots] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#f8faff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          fontSize: "36px",  
+          color: theme.palette.primary.main,  
+          fontWeight: "bold",  
+          marginBottom: "20px",
+        }}
+      >
+        Loading{dots}
+      </Box>
+      <CircularProgress
+        sx={{
+          color: theme.palette.primary.main,  
+        }}
+      />
+
+      <Box
+        sx={{
+          fontSize: "18px",
+          marginTop: "20px",
+          color: theme.palette.text.secondary,
+        }}
+      >
+        Please wait while the app is loading.
+      </Box>
+    </Box>
+  );
 };
 
 export default LoadingScreen;
