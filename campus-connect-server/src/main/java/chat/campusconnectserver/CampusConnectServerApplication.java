ecc422f709e729e2adf74f66f11939a715501e04
janina280 <coceijanina@yahoo.com>
2025-01-11 13:07:09 +0200

    fix

--- campus-connect-server/src/main/java/chat/campusconnectserver/CampusConnectServerApplication.java
+++ campus-connect-server/src/main/java/chat/campusconnectserver/CampusConnectServerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CampusConnectServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CampusConnectServerApplication.class, args);
+        SpringApplication app = new SpringApplication(CampusConnectServerApplication.class);
+        app.setAdditionalProfiles("dev");
+        app.run(args);
     }
 
 }
